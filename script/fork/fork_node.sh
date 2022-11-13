57b2667f6f441966a2fb6b9bb1905736a6f6fb88
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2022-11-13 13:28:26 +0800

    update fork scripts

--- script/fork/fork_node.sh
+++ script/fork/fork_node.sh
@@ -4,4 +4,5 @@ SOURCE_DIR="$(dirname "$0")/"
 
 source "$SOURCE_DIR../../.env"
 
-anvil --fork-url $FORK_URL
+anvil --fork-url $FORK_PROVIDER_URL `### RPC URL of the blockchain to fork ###` \
+      --fork-block-number $FORK_BLOCK_NUMBER `### Block number of the blockchain to fork ###` \
--- script/fork/fork_test.sh
+++ script/fork/fork_test.sh
@@ -5,5 +5,7 @@ SOURCE_DIR="$(dirname "$0")/"
 source "$SOURCE_DIR../../.env"
 
 forge test \
-      --fork-url $FORK_URL \
-      --gas-report
+      --fork-url $FORK_PROVIDER_URL `### RPC URL of the blockchain to fork ###` \
+      --fork-block-number $FORK_BLOCK_NUMBER `### Block number of the blockchain to fork ###` \
+      -vvv `### (Verbosity) -vv: print console.log's, -vvv: print trace for failing tests, -vvvv: print trace for ALL tests ###` \
+      `### --gas-report ### Turns on analysis for function gas costs ###` \
